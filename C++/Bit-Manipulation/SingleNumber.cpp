9d80daa3f67984c377a1137f8a813b5863d1a9fc
Nguyễn Văn Nguyên <brightsmileslikesunshine@gmail.com>
2024-12-17 17:02:47 +0700

    Update SingleNumber.cpp

--- C++/Bit-Manipulation/SingleNumber.cpp
+++ C++/Bit-Manipulation/SingleNumber.cpp
@@ -1,1 +1,29 @@
+//You are given a non-empty array integers 'nums'. Every integer appears twice except for one 
+//Return the integer that appears only once.
+//Time complexity O(n) || Space complexity O(1)
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter the size of array: ";
+    cin >> size;
+    vector<int> nums(size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter " << i + 1 << "th element of array: ";
+        cin >> nums[i];
+    }
+
+    int singleNumber = 0;
+    for (int num : nums)
+    {
+        singleNumber ^= num; //Perform XOR operation with all elements in the array.
+    }
+    cout << "The integer that appears only once: " << singleNumber;
+
+    return 0;
+}

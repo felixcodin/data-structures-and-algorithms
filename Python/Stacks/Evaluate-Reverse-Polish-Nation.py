9e0f122f48ebefb5c999dc26bc94d29e3f653bfe
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-01-20 15:14:14 +0700

    Add Evaluate-Reverse-Polish-Nation.py file

--- Python/Stacks/Evaluate-Reverse-Polish-Nation.py
+++ Python/Stacks/Evaluate-Reverse-Polish-Nation.py
@@ -1,1 +1,24 @@
+#You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
+#Time complexity: O(n) || Space complexity: O(n)
 
+def evalRPN(tokens):
+    stack = []
+    for c in tokens:
+        if c == '+':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(a + b))
+        elif c == '-':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(a - b))
+        elif c == '*':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(a * b))
+        elif c == '/':
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(a // b))
+        else:
+            stack.append(int(c))
+    return stack.pop()
+
+tokens=["2","1","+","3","*"]
+print(evalRPN(tokens))

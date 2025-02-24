5f29d7d63de4679c4894a370b1090a48839a435c
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-02-24 17:31:13 +0700

    Add Valid-Sudoku.py file.

---
+++ Python/Arrays/Valid-Sudoku.py
@@ -0,0 +1,41 @@
+#You are given a a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+#1. Each row must contain the digits 1-9 without duplicates.
+#2. Each column must contain the digits 1-9 without duplicates.
+#3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+
+
+def validSudoku(board):
+    
+    from collections import defaultdict
+
+    rows = defaultdict(set)
+    cols = defaultdict(set)
+    squares = defaultdict(set)
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == ".":
+                continue
+            if (board[r][c] in rows[r] or
+                board[r][c] in cols[c] or
+                board[r][c] in squares[(r // 3, c // 3)]):
+                return False
+
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[(r // 3, c // 3)].add(board[r][c])
+
+    return True
+
+board=[[".",".","4",".",".",".","6","3","."],
+       [".",".",".",".",".",".",".",".","."],
+       ["5",".",".",".",".",".",".","9","."],
+       [".",".",".","5","6",".",".",".","."],
+       ["4",".","3",".",".",".",".",".","1"],
+       [".",".",".","7",".",".",".",".","."],
+       [".",".",".","5",".",".",".",".","."],
+       [".",".",".",".",".",".",".",".","."],
+       [".",".",".",".",".",".",".",".","."]]
+
+print(validSudoku(board))

3e96fc209f155ce26a7e0ad0ae4a203521b61ae0
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-03-05 21:28:57 +0700

    Update combination_sum_II.py file

--- Python/Arrays/combination_sum_II.py
+++ Python/Arrays/combination_sum_II.py
@@ -2,22 +2,26 @@
 #Each number in candidates may only be used once in the combination.
 #Note: The solution set must not contain duplicate combinations.
 
-def combinationSum2(candidates, target):
-    candidates.sort()
-    res = []
+class Solution:
+    def combinationSum2(self, candidates, target):
+        result = []
+        candidates.sort()
 
-    def backtrack(start, current_combination, current_sum):
-        if current_sum > target:
-            return
-        if current_sum == target:
-            res.append(current_combination[:])
-            return
-        for i in range(start, len(candidates)):
-            if i > start and candidates[i] == candidates[i - 1]:
-                continue
-            current_combination.append(candidates[i])
-            backtrack(i + 1, current_combination, current_sum + candidates[i])
-            current_combination.pop()
-
-    backtrack(0, [], 0)
-    return res
+        def backtrack(start, current_combination, current_sum):
+            if current_sum == target:
+                result.append(current_combination[:])
+                return
+            if current_sum > target:
+                return
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                current_combination.append(candidates[i])
+                backtrack(i + 1, current_combination, current_sum + candidates[i])
+                current_combination.pop()
+        
+        backtrack(0, [], 0)
+        return result
+            

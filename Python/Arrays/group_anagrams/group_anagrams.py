3d944a1c46cf6f88d8d7f205edc0582ca4dd36a0
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-03-06 21:20:17 +0700

    Update group_anagrams.py file

--- Python/Arrays/group_anagrams/group_anagrams.py
+++ Python/Arrays/group_anagrams/group_anagrams.py
@@ -2,18 +2,12 @@
 
 from collections import defaultdict
 
-def groupAnagrams(strs):
-    res = defaultdict(list) #create hash map stores `list` key value
-    for s in strs:
-        count = [0] * 26 #create a list containing the number of each character
-        for c in s:
-            count[ord(c) - ord('a')] += 1 #count the number of letter from a to z
-        res[tuple(count)].append(s) #convert `count` list into a tuple and used as a key in the dictionary `res`. Add string `s` to the list res[tuple(count)] (with the same "fingerprint")
-    return list(res.values())
-
-test_case_1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
-print(groupAnagrams(test_case_1))
-test_case_2 = ["x"]
-print(groupAnagrams(test_case_2))
-test_case_3 = [""]
-print(groupAnagrams(test_case_3))
+class Solution:
+    def groupAnagrams(self, strs):
+        res = defaultdict(list) #create hash map stores `list` key value
+        for s in strs:
+            count = [0] * 26 #create a list containing the number of each character
+            for c in s:
+                count[ord(c) - ord('a')] += 1 #count the number of letter from a to z
+            res[tuple(count)].append(s) #convert `count` list into a tuple and used as a key in the dictionary `res`. Add string `s` to the list res[tuple(count)] (with the same "fingerprint")
+        return list(res.values())

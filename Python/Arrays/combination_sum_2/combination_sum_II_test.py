c8e6aa3059021bb5f97002f53458fdf3eee0ace2
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-03-06 11:48:30 +0700

    add Folder for all files

--- Python/Arrays/combination_sum_2/combination_sum_II_test.py
+++ Python/Arrays/combination_sum_2/combination_sum_II_test.py
@@ -4,47 +4,47 @@ from combination_sum_II import Solution
 class TestCombinationSum2(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
-    def test_example_case(self):
+    def testExampleCase(self):
         candidates = [10, 1, 2, 7, 6, 1, 5]
         target = 8
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
-    def test_single_element(self):
+    def testSingleElement(self):
         candidates = [5]
         target = 5
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[5]])
-    def test_no_possible(self):
+    def testNoPossible(self):
         candidates = [3]
         target = 5
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [])
-    def test_multiple_element(self):
+    def testMultipleElement(self):
         candidates = [2, 5, 2, 1, 2]
         target = 5
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[1, 2, 2], [5]])        
-    def test_all_elements_are_the_same(self):
+    def testAllElementAreTheSame(self):
         candidates = [1, 1, 1, 1, 1]
         target = 3
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[1, 1, 1]])
-    def test_large_target_with_multiple_combinations(self):
+    def testLargeTargetWithMultipleCombinations(self):
         candidates = [10, 1, 2, 7, 6, 1, 5]
         target = 15
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[1, 1, 2, 5, 6], [1, 1, 6, 7], [1, 2, 5, 7], [2, 6, 7], [5, 10]])
-    def test_empty_candidates_array(self):
+    def testEmptyCandidatesArray(self):
         candidates = []
         target = 3
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [])
-    def test_target_is_zero(self):
+    def testTargetIsZero(self):
         candidates = [1, 2, 3]
         target = 0
         result = self.solution.combinationSum2(candidates, target)
         self.assertEqual(result, [[]])
-    def test_large_numbers_in_candidates(self):
+    def testLargeNumberInCandidates(self):
         candidates = [10, 20, 30, 40]
         target = 30
         result = self.solution.combinationSum2(candidates, target)

b593935e566b2c8a4bbe7bc65d310ffffe41a36b
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-03-08 13:50:16 +0700

    Update

--- Python/Arrays/valid_sudoku/valid_sudoku.py
+++ Python/Arrays/valid_sudoku/valid_sudoku.py
@@ -4,38 +4,25 @@
 #2. Each column must contain the digits 1-9 without duplicates.
 #3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
 
+class Solution:
+    def validSudoku(self, board):
+        from collections import defaultdict
 
-def validSudoku(board):
-    
-    from collections import defaultdict
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
 
-    rows = defaultdict(set)
-    cols = defaultdict(set)
-    squares = defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
 
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == ".":
-                continue
-            if (board[r][c] in rows[r] or
-                board[r][c] in cols[c] or
-                board[r][c] in squares[(r // 3, c // 3)]):
-                return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
-            cols[c].add(board[r][c])
-            rows[r].add(board[r][c])
-            squares[(r // 3, c // 3)].add(board[r][c])
-
-    return True
-
-board=[[".",".","4",".",".",".","6","3","."],
-       [".",".",".",".",".",".",".",".","."],
-       ["5",".",".",".",".",".",".","9","."],
-       [".",".",".","5","6",".",".",".","."],
-       ["4",".","3",".",".",".",".",".","1"],
-       [".",".",".","7",".",".",".",".","."],
-       [".",".",".","5",".",".",".",".","."],
-       [".",".",".",".",".",".",".",".","."],
-       [".",".",".",".",".",".",".",".","."]]
-
-print(validSudoku(board))
+        return True
